f60a80aee06105631f1f67e0bf0cec45c657311a
pauled <drpauled@gmail.com>
2026-03-02 12:38:29 -0500

    week 7-1a

---
+++ src/week7polymorphism/PolyExample.java
@@ -0,0 +1,18 @@
+package week7polymorphism;
+
+import week6inheritance.HealthPotion;
+import week6inheritance.Location;
+import week6inheritance.Usable;
+import week6inheritance.Weapon;
+
+import java.util.ArrayList;
+
+public class PolyExample {
+    public static void main(String[] args) {
+        ArrayList<Usable> inventory=new ArrayList<>();
+        Usable l=new HealthPotion(1,2,10);
+        inventory.add(l);
+        l=new Weapon(3,4,5);
+        inventory.add(l);
+    }
+}
