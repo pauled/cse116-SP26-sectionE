20d0052d88d71ee9e16cf97ebea3b2d94ab52cb6
pauled <drpauled@gmail.com>
2026-03-11 12:52:15 -0400

    week 8-2b

--- src/week8testing/CodeToTest.java
+++ src/week8testing/CodeToTest.java
@@ -9,28 +9,113 @@ import static org.junit.Assert.assertTrue;
 
 public class CodeToTest {
     public static int subtract(int a,int b){
-        return a-b;
+        return a-Math.abs(b);
     }
     @Test
-    public void testSubtract(){
+    public void testSubtractPositiveInputsFirstLarger(){
         ArrayList<TestSubtractHelper> tests=new ArrayList<>();
         tests.add(new TestSubtractHelper(8,5,3));
-        tests.add(new TestSubtractHelper(0,7,-6));
-        tests.add(new TestSubtractHelper(8,2,6));
+        tests.add(new TestSubtractHelper(10,2,8));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testSubtractPositiveInputsSecondLarger(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(6,8,-2));
+        tests.add(new TestSubtractHelper(1,18,-17));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testSubtractPositiveInputsEqualValue(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(8,8,0));
 
         for (TestSubtractHelper test : tests){
             test.runTest();
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(subtract(8,5));
-        System.out.println(subtract(0,7));
-        System.out.println(subtract(8,2));
-        if (subtract(8,5)==3){
-            System.out.println("passed subtract(8,5)==3");
-        } else {
-            System.out.println("failed subtract(8,5)==3");
+    @Test
+    public void testSubtractNegativesInputsNegativeFirst(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(-8,8,-16));
+        tests.add(new TestSubtractHelper(-6,8,-14));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
         }
+    }
+    @Test
+    public void testSubtractNegativesInputsNegativeSecond(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(8,-5,13));
+        tests.add(new TestSubtractHelper(10,-22,32));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testSubtractNegativesInputsBothNegative(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(-8,-5,-3));
+        tests.add(new TestSubtractHelper(-6,-8,2));
+        tests.add(new TestSubtractHelper(-8,-8,0));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testSubtractZerosInputsZeroSecond(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(8,0,8));
+        tests.add(new TestSubtractHelper(-6,0,-6));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testSubtractZerosInputsZeroFirst(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(0,22,-22));
+        tests.add(new TestSubtractHelper(0,-8,8));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testSubtractZerosInputsZeroBoth(){
+        ArrayList<TestSubtractHelper> tests=new ArrayList<>();
+        tests.add(new TestSubtractHelper(0,0,0));
+
+        for (TestSubtractHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testStrings(){
+        String a="hello ";
+        String b="goodbye";
+        String c=a+b;
+        System.out.println(c);
+        System.out.println("hello goodbye".equals(c));
+    }
+    @Test
+    public void testDoubles(){
+        double total=0;
+        for (int x=0;x<100;x++){
+            total+=.1;
+        }
+        System.out.println(total);
+        assertEquals("this shows a delta",10,total,.001);
     }
 }
